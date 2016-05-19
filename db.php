13b363c8070638ff93317830b142a73b52e0de98
bpeterson1663 <bpeterson1663@gmail.com>
2016-05-19 10:29:56 -0500

    db update

--- db.php
+++ db.php
@@ -3,13 +3,16 @@
 // $user = "root";
 // $password = "root";
 // $db = "chat";
+//$link = mysqli_connect($host, $user, $password, $db);
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$host = 'us-cdbr-iron-east-04';
-$user = "bdbe1bd843a3e7";
-$password = "c029cf19";
-$db = "heroku_1122e83085eb00f";
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
 
-$link = mysqli_connect($host, $user, $password, $db);
+$link = new mysqli($server, $username, $password, $db);
+
 
 function formatDate($date){
   return date('g:i a', strtotime($date));
