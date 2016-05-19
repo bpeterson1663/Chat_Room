32a389ff9597b9c7ea2cfe1b87a8838fd1f0e7df
bpeterson1663 <bpeterson1663@gmail.com>
2016-05-19 14:57:48 -0500

    db update

--- db.php
+++ db.php
@@ -13,9 +13,9 @@
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $server = $url["us-cdbr-iron-east-04.cleardb.net"];
-$username = $url["bdbe1bd843a3e7"];
-$password = $url["c029cf19"];
-$db = substr($url["heroku_1122e83085eb00f"], 1);
+$username = $url["b03c8432abf7f1"];
+$password = $url["84d7bfa6"];
+$db = substr($url["heroku_1a4d0911e825216"], 1);
 
 $link = mysqli_connect($server, $username, $password, $db);
 
