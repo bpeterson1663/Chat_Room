602b7d66d97f5e86a1db9ee1532f27a66d1bc333
bpeterson1663 <bpeterson1663@gmail.com>
2016-05-18 22:17:18 -0500

    DB update

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "root";
-$db = "chat";
+$host = "mysql://bdbe1bd843a3e7:c029cf19@us-cdbr-iron-east-04.cleardb.net/heroku_1122e83085eb00f?reconnect=true";
+$user = "bdbe1bd843a3e7";
+$password = "c029cf19";
+$db = "heroku_1122e83085eb00f";
 
 $link = mysqli_connect($host, $user, $password, $db);
 
