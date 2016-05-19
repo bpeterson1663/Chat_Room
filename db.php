bd52ad8e35d17c15b9be35f3afc9d3ca0bb8dfb4
bpeterson1663 <bpeterson1663@gmail.com>
2016-05-19 10:40:54 -0500

    db update

--- db.php
+++ db.php
@@ -4,12 +4,18 @@
 // $password = "root";
 // $db = "chat";
 //$link = mysqli_connect($host, $user, $password, $db);
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
+$host = 'us-cdbr-iron-east-04.cleardb.net';
+$user = "bdbe1bd843a3e7";
+$password = "c029cf19";
+$db = "heroku_1122e83085eb00f";
+// 
+// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+//
+// $server = $url["host"];
+// $username = $url["user"];
+// $password = $url["pass"];
+// $db = substr($url["path"], 1);
 
 $link = mysqli_connect($server, $username, $password, $db);
 
