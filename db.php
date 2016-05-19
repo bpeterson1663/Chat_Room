0269828972d9e99a7fa853750ec2e7167cbfc6b2
bpeterson1663 <bpeterson1663@gmail.com>
2016-05-18 22:21:57 -0500

    DB Update

--- db.php
+++ db.php
@@ -1,5 +1,6 @@
 <?php
-$host = "mysql://bdbe1bd843a3e7:c029cf19@us-cdbr-iron-east-04.cleardb.net/heroku_1122e83085eb00f?reconnect=true";
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$host = $url['host'];
 $user = "bdbe1bd843a3e7";
 $password = "c029cf19";
 $db = "heroku_1122e83085eb00f";
