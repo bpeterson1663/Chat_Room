d7ea71cedbf55d4356949ad2c569d0f4a9851e42
bpeterson1663 <bpeterson1663@gmail.com>
2016-06-13 22:40:34 -0500

    DB update

--- chat.php
+++ chat.php
@@ -1,5 +1,17 @@
 <?php
-include('db.php');
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["us-cdbr-iron-east-04.cleardb.net"];
+$username = $url["b03c8432abf7f1"];
+$password = $url["84d7bfa6"];
+$db = substr($url["heroku_1a4d0911e825216"], 1);
+
+$link = mysqli_connect($server, $username, $password, $db);
+
+
+function formatDate($date){
+  return date('g:i a', strtotime($date));
+}
 $query = "CREATE TABLE IF NOT EXISTS chat_data (id SERIAL PRIMARY KEY, name varchar(30), message text, date timestamp(current_timestamp) );";
 mysqli_query($link, $query);
 $query = "SELECT * FROM chat_data ORDER BY id DESC;";
--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
-  include('db.php');
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["us-cdbr-iron-east-04.cleardb.net"];
+$username = $url["b03c8432abf7f1"];
+$password = $url["84d7bfa6"];
+$db = substr($url["heroku_1a4d0911e825216"], 1);
+
+$link = mysqli_connect($server, $username, $password, $db);
+
+
+function formatDate($date){
+  return date('g:i a', strtotime($date));
+}
  ?>
 
 <!DOCTYPE html>
